7ca05fa088844254dfd9a7194880e188953a137e
Enlawn <abderahamane.belkacemi.g@gmail.com>
2025-11-08 13:13:00 +0100

    correction + 1er version fonctionnel

--- Test-site/dist/main.js
+++ Test-site/dist/main.js
@@ -1,8 +1,8 @@
+"use strict";
 // const bouton = document.querySelector("#btn") as HTMLButtonElement | null;
 // const message = document.querySelector("#message") as HTMLParagraphElement | null;
-
+Object.defineProperty(exports, "__esModule", { value: true });
 // let compteur = 0;
-
 // if (bouton && message) {
 //   bouton.addEventListener("click", () => {
 //     compteur++;
@@ -11,18 +11,13 @@
 // } else {
 //   console.error("Éléments HTML introuvables.");
 // }
-
-
-
-const button = document.getElementById("btn") as HTMLButtonElement;
-const message = document.getElementById("message") as HTMLParagraphElement;
+const button = document.getElementById("btn");
+const message = document.getElementById("message");
 let compteur = 0;
-
-button.onclick = function() {
+button.onclick = function () {
     compteur++;
-}
-
-button.onclick = function() {
+};
+button.onclick = function () {
     message.innerHTML = `tu as cliqué ${compteur} fois !`;
-}
-
+};
+//# sourceMappingURL=main.js.map
