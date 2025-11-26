a657dd6987c9ac55747f0c0c5577eb17bcd8d340
Enlawn <abderahamane.belkacemi.g@gmail.com>
2025-11-26 10:24:27 +0100

    simplification et correction
    je suis en train d'apprendre, alors pourquoi faire compliquer quand je peux commencer simple et améliorer progressivement l'apparence et le comportement du site ~

--- Test-site/dist/main.js
+++ Test-site/dist/main.js
@@ -2,17 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const button = document.getElementById("btn");
 const message = document.getElementById("message");
-const btn2 = document.getElementById("btn2");
-const btn3 = document.getElementById("btn3");
-const windowsmenu = document.getElementById("windowsmenu");
-const linuxmenu = document.getElementById("linuxmenu");
-btn2.addEventListener("click", () => {
-    windowsmenu.classList.toggle("hidden");
-});
-btn3.addEventListener("click", () => {
-    linuxmenu.classList.toggle("hidden");
-});
-btn2.addEventListener("click", () => {
-    console.log("Bouton 2 cliqué !");
-});
+const btn1 = document.getElementById("menu1-btn");
+const menu1content = document.getElementById("menu1-btn-content");
+const btn2 = document.getElementById("menu2-btn");
+const menu2content = document.getElementById("menu2-btn-content");
 //# sourceMappingURL=main.js.map
--- Test-site/src/main.ts
+++ Test-site/src/main.ts
@@ -6,20 +6,9 @@ const button = document.getElementById("btn") as HTMLButtonElement;
 const message = document.getElementById("message") as HTMLParagraphElement;
 
 
-const btn2 = document.getElementById("btn2") as HTMLImageElement;
-const btn3 = document.getElementById("btn3") as HTMLImageElement;
+const btn1 = document.getElementById("menu1-btn") as HTMLButtonElement;
+const menu1content = document.getElementById("menu1-btn-content") as HTMLParagraphElement;
 
-const windowsmenu = document.getElementById("windowsmenu") as HTMLDivElement;
-const linuxmenu = document.getElementById("linuxmenu") as HTMLDivElement;
+const btn2 = document.getElementById("menu2-btn") as HTMLButtonElement;
+const menu2content = document.getElementById("menu2-btn-content") as HTMLDivElement;
 
-btn2.addEventListener("click", () => {
-    windowsmenu.classList.toggle("hidden");
-});
-
-btn3.addEventListener("click", () => {
-    linuxmenu.classList.toggle("hidden");
-});
-
-btn2.addEventListener("click", () => {
-  console.log("Bouton 2 cliqué !");
-});
