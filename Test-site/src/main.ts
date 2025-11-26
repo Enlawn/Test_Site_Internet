da9ffe7a6b3dad9ba5d0615c562c2646edfcce87
Enlawn <abderahamane.belkacemi.g@gmail.com>
2025-11-26 14:16:34 +0100

    menu dynamique

--- Test-site/src/main.ts
+++ Test-site/src/main.ts
@@ -7,7 +7,7 @@ const message = document.getElementById("message") as HTMLParagraphElement;
 
 
 const btn1 = document.getElementById("menu1-btn") as HTMLButtonElement;
-const menu1content = document.getElementById("menu1-btn-content") as HTMLParagraphElement;
+const menu1content = document.getElementById("menu1-btn-content") as HTMLDivElement;
 
 const btn2 = document.getElementById("menu2-btn") as HTMLButtonElement;
 const menu2content = document.getElementById("menu2-btn-content") as HTMLDivElement;
